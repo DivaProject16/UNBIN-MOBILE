70b5c5eec11e9c9aca2a7eb480a9a8693b410e68
Diva Rizqa Maharani <divarizqa167@gmail.com>
2022-12-15 16:20:05 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public allData:any = [];
+  constructor(private route: Router) {
+  }
 
+  Logout(){
+    this.route.navigate(['/tab1'])
+  }
+
+
+    
 }
